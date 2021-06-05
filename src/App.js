317aed6a191caa5af045432693153fbf446f5048
Abu Taher <abut70243@gmail.com>
2021-06-05 22:53:21 +0600

    readme file edited

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import {
   Route
 } from "react-router-dom";
 import AccountSettings from "./components/AcountSettings/AccountSettings";
-import DailyGoal from "./components/DailyGoal/DailyGoal";
 import Home from "./components/Home/Home";
 
 export default function App() {
   return (
     <Router>
         <Switch>
-          <Route path="/dailyGoal">
-            <DailyGoal />
-          </Route>
           <Route path="/accountSettings">
             <AccountSettings></AccountSettings>
           </Route>
